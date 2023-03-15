8574f192cc5f5303338224ec8691dcbeb800e754
IsabellaOH <isabellaohara@btinternet.com>
2023-03-15 15:15:23 +0000

    page titles

--- spec/entries/entry_e2e_spec.rb
+++ spec/entries/entry_e2e_spec.rb
@@ -7,30 +7,35 @@ RSpec.describe 'end2end create entry', type: :system do
 
     it 'logs in and shows home page' do
         visit root_path
+        sleep 1
         expect(page).to have_content('Log In')
         fill_in 'username', with: "test"
         fill_in 'password', with: "testpassword"
+        sleep 1
         click_link 'Log in'
         expect(page).to have_content('All Entries')
+        sleep 1
     end
 
     it 'shows the all notes page' do
         visit welcome_index_path
         expect(page).to have_content('All Entries')
         find(:xpath, '/html/body/div/div[3]/div/ul/li[2]/strong/a').click
+        sleep 1
         expect(page).to have_content('All Notes')
+        sleep 1
     end
 
     it 'shows new entry page and creates entry' do
         visit entries_path
-        sleep 2
         click_link 'New note'
         expect(page).to have_content('Create a new note')
         fill_in 'Name', with: "test"
         fill_in 'Link', with: "www.test.com"
         fill_in 'Notes', with: "test notes"
-        sleep 2
+        sleep 1
         click_button 'Create new post'
         expect(page).to have_content('test')
+        sleep 1
     end
 end
